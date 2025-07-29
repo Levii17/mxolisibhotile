e480d6f2d1055f82cf32bcfcc2cda13f12746243
Mxolisi <mxolisibhotile58@gmail.com>
2025-07-29 20:25:00 +0200

    Fixed three js import

--- mxolisibhotile/public/assets/js/model.js
+++ mxolisibhotile/public/assets/js/model.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const canvas = document.querySelector('#three-canvas');
